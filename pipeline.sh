42c08b9713ca67a51fc5143ef037b1eacd34902b
Neil Williams <neil.williams@linaro.org>
2014-11-25 13:32:15 +0000

    workaround pending file location issues in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -2,6 +2,23 @@
 
 # set -e
 
+# This script runs the dispatcher refactoring code on the command line.
+# The output remains confined to local directories due to several reasons:
+# 1. This is on the dispatcher, so there is no external apache visibility
+# 2. The web frontend has no devices, so cannot run the tests
+# 3. The sshfs is sufficiently secure that symlinks result in a directory
+#    from which apache cannot serve files, even if the symlink is allowed.
+# 4. The sshfs requires sudo su lavaserver -c "command" to use
+# 5. After copying to sshfs, the remote web frontend would have to *copy*
+#    the files to make those visible.
+# 6. One other way would be dummy-host but that's confusing as it merges
+#    the new dispatcher output with the old dispatcher output in django.
+
+# Secondary issue, which can be fixed in master, is that the device
+# configuration needs some hand-holding currently. The format of the files
+# has changed but the current code does not look into paths in /etc/, only
+# /usr/lib/python2.7/dist-packages/
+
 START=1416310894
 
 DATE=`date +%s`
@@ -12,7 +29,26 @@ mkdir $OUTPUT
 sudo touch $OUTPUT/console.log
 sudo chmod 666 $OUTPUT/console.log
 sudo lava-dispatch --target kvm01 ./local-kvm.yaml --output-dir=$OUTPUT | tee $OUTPUT/console.log
-rsync -az $OUTPUT .
+rsync -az $OUTPUT ..
+
+#panda
+
+DATE=`date +%s`
+ID=`expr $DATE - $START`
+OUTPUT="/tmp/panda-ramdisk-$ID"
+mkdir $OUTPUT
+sudo touch $OUTPUT/console.log
+if [ ! -f "/usr/lib/python2.7/dist-packages/lava_dispatcher/pipeline/devices/panda-01.conf" ]; then
+	sudo cp ./panda-01.conf /usr/lib/python2.7/dist-packages/lava_dispatcher/pipeline/devices/
+fi
+sudo lava-dispatch --target panda-01 ./panda-ramdisk.yaml --output-dir=$OUTPUT | tee $OUTPUT/console.log
+rsync -az $OUTPUT ..
+if [ -f "/usr/lib/python2.7/dist-packages/lava_dispatcher/pipeline/devices/panda-01.conf" ]; then
+	sudo rm /usr/lib/python2.7/dist-packages/lava_dispatcher/pipeline/devices/panda-01.conf
+fi
+
+# beaglebone blacks not available yet.
+exit 0
 
 DATE=`date +%s`
 ID=`expr $DATE - $START`
@@ -21,7 +57,7 @@ mkdir $OUTPUT
 sudo touch $OUTPUT/console.log
 sudo chmod 666 $OUTPUT/console.log
 sudo lava-dispatch --target bbb-01 ./local-uboot-ramdisk.yaml --output-dir=$OUTPUT | tee $OUTPUT/console.log
-rsync -az $OUTPUT .
+rsync -az $OUTPUT ..
 
 DATE=`date +%s`
 ID=`expr $DATE - $START`
@@ -30,15 +66,4 @@ mkdir $OUTPUT
 sudo touch $OUTPUT/console.log
 sudo chmod 666 $OUTPUT/console.log
 sudo lava-dispatch --target bbb-01 ./local-uboot-pipeline.yaml --output-dir=$OUTPUT | tee $OUTPUT/console.log
-rsync -az $OUTPUT .
-
-exit 0
-
-DATE=`date +%s`
-ID=`expr $DATE - $START`
-OUTPUT="/tmp/panda-ramdisk-$ID"
-mkdir $OUTPUT
-sudo touch $OUTPUT/console.log
-sudo lava-dispatch --target panda-es-01 ./panda-ramdisk.yaml --output-dir=$OUTPUT | tee $OUTPUT/console.log
-rsync -az $OUTPUT .
-
+rsync -az $OUTPUT ..
