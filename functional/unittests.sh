e00552bbe1922e27e29b2abb64c3cb6621927d02
Neil Williams <neil.williams@linaro.org>
2016-01-27 11:20:47 +0000

    drop fixupdict and do the result parsing in the custom script
    Change-Id: Iaf33f1a7555dd53b6723e7c6a931a99ed12342ee

--- functional/unittests.sh
+++ functional/unittests.sh
@@ -10,7 +10,7 @@ fi
 results=`grep -E "Ran [0-9]{3} tests in " ci.log | sed -E 's/^Ran ([0-9]{3}) tests in ([0-9\.]+)s$/\1 \2/'`
 count=`echo $results|cut -d' ' -f 1`
 time=`echo $results|cut -d' ' -f 2`
-for line in `grep '\.\.\. ' ci.log |grep test | grep -v ok | awk '{print $1"_"$2":"$4}'`; do
+for line in `grep '\.\.\. ' ci.log |grep test | grep ok | awk '{print $1"_"$2":"$4}'`; do
 	NAME=`echo $line|cut -d: -f1`
 	if `grep ${line} ERROR 2>/dev/null`; then
 		lava-test-case "${NAME}" --result fail
