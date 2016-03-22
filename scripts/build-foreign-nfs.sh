05e332ae1433dafcf8cc2a8f4a5081d7234b5948
Neil Williams <neil.williams@linaro.org>
2016-03-22 16:52:45 +0000

    Generic foreign build script.
    Add a generic script for extracting initramfs, modules.tgz,
    vmlinuz and dtb from base debootstrap.
    
    Change-Id: Ia8afa7aea7ac3e9446ba160f5aea7b4d353d0e5b

---
+++ scripts/build-foreign-nfs.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Copyright 2016 Neil Williams <codehelp@debian.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+
+set -e
+set -x
+
+SUITE=jessie
+ARCH=arm64
+BIN_FMT=/usr/bin/qemu-aarch64-static
+MIRROR=http://mirror.bytemark.co.uk/debian
+
+while getopts "a:b:d:m:" opt; do
+  case $opt in
+    a)
+      ARCH=$OPTARG
+      ;;
+    b)
+      BIN_FMT=$OPTARG
+      ;;
+    d)
+      SUITE=$OPTARG
+      ;;
+    m)
+      MIRROR=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+TARGET=${SUITE}-${ARCH}
+NAME=${TARGET}
+
+rm -rf ${TARGET}/
+debootstrap --variant=minbase --foreign --arch=${ARCH} ${SUITE} ${TARGET} ${MIRROR}
+echo deb ${MIRROR} ${SUITE} main > ${TARGET}/etc/apt/sources.list
+echo ${SUITE} > ${TARGET}/etc/hostname
+cp ${BIN_FMT} ${TARGET}/usr/bin/
+chroot ${TARGET}/ debootstrap/debootstrap --second-stage
+chroot ${TARGET}/ passwd root -d
+chroot ${TARGET}/ apt -q update
+chroot ${TARGET}/ apt -y -q install linux-image-${ARCH}
+mv ${TARGET}/boot/* .
+mv ${TARGET}/usr/lib/linux-image-* .
+mv linux-image-* dtbs
+tar -C ${TARGET}/ -czf modules.tar.gz ./lib/modules/
+rm -rf ${TARGET}/lib/modules/
+tar -C ${TARGET}/ -czf ${NAME}-nfs.tar.gz .
+rm -rf ${TARGET}/
+ln -s initrd.img-*  initramfs.cpio.gz
+ln -s vmlinuz-* vmlinuz
+md5sum initramfs.cpio.gz > initramfs.cpio.gz.md5sum.txt
+sha256sum initramfs.cpio.gz > initramfs.cpio.gz.sha256sum.txt
+md5sum vmlinuz > vmlinuz.md5sum.txt
+sha256sum vmlinuz > vmlinuz.sha256sum.txt
+md5sum ${NAME}-nfs.tar.gz > ${NAME}-nfs.tar.gz.md5sum.txt
+sha256sum ${NAME}-nfs.tar.gz > ${NAME}-nfs.tar.gz.sha256sum.txt
+md5sum modules.tar.gz > modules.tar.gz.md5sum.txt
+sha256sum modules.tar.gz > modules.tar.gz.sha256sum.txt
+md5sum dtbs/* > dtbs.md5sum.txt
+sha256sum dtbs/* > dtbs.sha256sum.txt
