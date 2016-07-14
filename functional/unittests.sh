97e6e9b0dc6d77e4cdff56d08391c7c1396f27bf
Neil Williams <neil.williams@linaro.org>
2016-07-14 15:59:03 +0100

    improve pick of errors and output a fail if count or time is zero
    Change-Id: I01118a62ebe9ebe599f2f3c3f6e51c8c02d12993

--- functional/unittests.sh
+++ functional/unittests.sh
@@ -10,17 +10,25 @@ results=`grep -E "Ran [0-9]{3} tests in " ci.log | sed -E 's/^Ran ([0-9]{3}) tes
 count=`echo $results|cut -d' ' -f 1`
 time=`echo $results|cut -d' ' -f 2`
 for line in `grep '\.\.\. ' ci.log |grep test | grep ok | awk '{print $1"_"$2":"$4}'`; do
-	NAME=`echo $line|cut -d: -f1`
-	if `grep ${line} ERROR 2>/dev/null`; then
-		lava-test-case "${NAME}" --result fail
-	else
-		lava-test-case "${NAME}" --result pass
-	fi
+	NAME=`echo $line|cut -d: -f1| tr -d '()' `
+	lava-test-case "${NAME}" --result pass
+done
+for line in `grep '\.\.\. ' ci.log | grep ERROR | awk '{print $1"_"$2":"$4}'`; do
+	NAME=`echo $line|cut -d: -f1| tr -d '()' `
+	lava-test-case "${NAME}" --result fail
 done
 for line in `grep 'FAIL: test' ci.log|cut -d' ' -f2-`; do
 	lava-test-case ${line} --result fail
 done
-lava-test-case total-count --result pass --measurement ${count} --units tests
-lava-test-case overall-speed --result pass --measurement ${time} --units seconds
+if [ $count > 0 ]; then
+	lava-test-case total-count --result pass --measurement ${count} --units tests
+else
+	lava-test-case total-count --result fail
+fi
+if [ $time > 0 ]; then
+	lava-test-case overall-speed --result pass --measurement ${time} --units seconds
+else
+	lava-test-case overall-speed --result fail
+fi
 lava-test-case logfile --result pass
 lava-test-case-attach logfile ci.log text/plain
